ffdd1565291f04388e994584192f3043ea696a3e
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2026-06-01 22:25:41 -0500

    Implement basic arithmetic functions and operation handler

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello world")
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            return 0;
+    }
+}
